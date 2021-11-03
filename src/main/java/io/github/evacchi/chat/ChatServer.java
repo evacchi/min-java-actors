20275a596780701e45fe7467873a53439c1fc702
Edoardo Vacchi <evacchi@users.noreply.github.com>
2021-11-03 16:59:50 +0100

    jackson

--- src/main/java/io/github/evacchi/chat/ChatServer.java
+++ src/main/java/io/github/evacchi/chat/ChatServer.java
@@ -4,22 +4,20 @@ import io.github.evacchi.Actor;
 import io.github.evacchi.Actor.Address;
 import io.github.evacchi.Actor.Behavior;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.io.UncheckedIOException;
 import java.net.ServerSocket;
-import java.net.Socket;
 import java.util.ArrayList;
-import java.util.Scanner;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
 import static io.github.evacchi.Actor.Become;
 import static io.github.evacchi.Actor.Stay;
 import static io.github.evacchi.chat.ChatBehavior.*;
-import static java.lang.System.err;
 import static java.lang.System.out;
-import static java.util.concurrent.TimeUnit.SECONDS;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 public interface ChatServer {
     int portNumber = 4444;
@@ -28,7 +26,7 @@ public interface ChatServer {
     Actor.System io = new Actor.System(Executors.newCachedThreadPool());
     ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(4);
 
-    static void main(String[] args) throws IOException {
+    static void main(String... args) throws IOException {
         var serverSocket = new ServerSocket(portNumber);
         out.printf("Server started at %s.\n", serverSocket.getLocalSocketAddress());
 
@@ -42,27 +40,28 @@ public interface ChatServer {
         var clients = new ArrayList<Address>();
         return IOBehavior.of(msg -> {
             switch (msg) {
-                // broadcast the message to all connected clients
-                case Message m -> {
-                    clients.forEach(c -> c.tell(m));
-                    return Stay;
-                }
                 // create a connection handler and a message handler for that client
                 case CreateClient n -> {
                     var socket = n.socket();
                     out.println("accepts : " + socket.getRemoteSocketAddress());
 
-                    var in = new Scanner(socket.getInputStream());
+                    var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                     var clientSocketHandler =
-                            io.actorOf(me -> clientSocketHandler(me, self, in));
+                            io.actorOf(me -> clientInput(me, self, in));
 
                     var writer = new PrintWriter(socket.getOutputStream(), true);
                     var clientMessageHandler =
-                            sys.actorOf(client -> clientMessageHandler(writer));
+                            sys.actorOf(client -> clientOutput(writer));
 
                     clients.add(clientMessageHandler);
                     return Stay;
                 }
+                // broadcast the message to all connected clients
+                case Message m -> {
+                    clients.forEach(c -> c.tell(m));
+                    return Stay;
+                }
+                // ignore all other messages
                 default -> {
                     return Stay;
                 }
@@ -71,7 +70,7 @@ public interface ChatServer {
     }
 
     static Behavior serverSocketHandler(Address self, Address clientManager, ServerSocket serverSocket) {
-        scheduler.schedule(() -> self.tell(Poll), 1, SECONDS);
+        scheduler.schedule(() -> self.tell(Poll), 1000, MILLISECONDS);
         return IOBehavior.of(msg -> {
             if (msg != Poll) return Stay;
             var socket = serverSocket.accept();
@@ -80,26 +79,33 @@ public interface ChatServer {
         });
     }
 
-    static Behavior clientSocketHandler(Address self, Address server, Scanner in) {
-        scheduler.schedule(() -> self.tell(Poll), 1, SECONDS);
+    static Behavior clientInput(Address self, Address clientManager, BufferedReader in) {
+        // schedule a message to self
+        scheduler.schedule(() -> self.tell(Poll), 100, MILLISECONDS);
 
-        return msg -> {
+        return IOBehavior.of(msg -> {
+            // ignore non-Poll messages
             if (msg != Poll) return Stay;
-            if (in.hasNextLine()) {
-                var input = in.nextLine();
+            if (in.ready()) {
+                var input = in.readLine();
+                // log message to stdout
                 out.println(input);
-                server.tell(new Message(input));
+                // deserialize from JSON
+                Message m = Mapper.readValue(input, Message.class);
+                // broadcast to all other clients
+                clientManager.tell(m);
             }
-            return Become(clientSocketHandler(self, server, in));
-        };
 
+            // "stay" in the same state, ensuring that the initializer is re-evaluated
+            return Become(clientInput(self, clientManager, in));
+        });
     }
 
-    static Behavior clientMessageHandler(PrintWriter writer) {
-        return msg -> {
-            if (msg instanceof Message m) writer.println(m.text());
+    static Behavior clientOutput(PrintWriter writer) {
+        return IOBehavior.of(msg -> {
+            if (msg instanceof Message m) writer.println(Mapper.writeValueAsString(m));
             return Stay;
-        };
+        });
     }
 
 
--- src/main/java/io/github/evacchi/chat/Client.java
+++ src/main/java/io/github/evacchi/chat/Client.java
@@ -3,20 +3,20 @@ package io.github.evacchi.chat;
 import io.github.evacchi.Actor;
 import io.github.evacchi.chat.ChatBehavior.Message;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.Scanner;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.function.Consumer;
 
 import static io.github.evacchi.Actor.Become;
 import static io.github.evacchi.Actor.Stay;
 import static io.github.evacchi.chat.ChatBehavior.Poll;
 import static java.lang.System.in;
 import static java.lang.System.out;
-import static java.util.concurrent.TimeUnit.SECONDS;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 public interface Client {
 
@@ -28,40 +28,47 @@ public interface Client {
     static void main(String[] args) throws IOException {
         var userName = args[0];
 
-            var socket = new Socket(host, portNumber);
-            var userInput = new Scanner(in);
-            var socketInput = new Scanner(socket.getInputStream());
-            var socketOutput = new PrintWriter(socket.getOutputStream(), true);
+        var socket = new Socket(host, portNumber);
+        var userInput = new BufferedReader(new InputStreamReader(in));
+        var socketInput = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        var socketOutput = new PrintWriter(socket.getOutputStream(), true);
 
-            out.printf("Login........%s\n", userName);
-            out.printf("Local Port...%d\n", socket.getLocalPort());
-            out.printf("Server.......%s\n", socket.getRemoteSocketAddress());
+        out.printf("Login........%s\n", userName);
+        out.printf("Local Port...%d\n", socket.getLocalPort());
+        out.printf("Server.......%s\n", socket.getRemoteSocketAddress());
 
-            var serverOut = sys.actorOf(self -> msg -> {
-                if (msg instanceof Message m)
-                    socketOutput.println(userName + " > " + m.text());
-                return Stay;
-            });
-            var userIn = sys.actorOf(self -> lineReader(self,
-                    userInput,
-                    line -> serverOut.tell(new Message(line))));
-            var serverSocketReader = sys.actorOf(self -> lineReader(self,
-                    socketInput,
-                    out::println));
+        var serverOut = sys.actorOf(self -> ChatBehavior.IOBehavior.of(msg -> {
+            if (msg instanceof Message m)
+                socketOutput.println(ChatBehavior.Mapper.writeValueAsString(m));
+            return Stay;
+        }));
+        var userIn = sys.actorOf(self -> lineReader(self,
+                userInput,
+                line -> serverOut.tell(new Message(userName, line))));
+        var serverSocketReader = sys.actorOf(self -> lineReader(self,
+                socketInput,
+                line -> {
+                    Message message = ChatBehavior.Mapper.readValue(line, Message.class);
+                    out.printf("%s > %s\n", message.user(), message.text());
+                }));
 
     }
 
-    static Actor.Behavior lineReader(Actor.Address self, Scanner in, Consumer<String> lineConsumer) {
-        scheduler.schedule(() -> self.tell(Poll), 1, SECONDS);
-        return msg -> {
-            if (msg != Poll) return Stay;
 
-            if (in.hasNextLine()) {
-                var input = in.nextLine();
+    static Actor.Behavior lineReader(Actor.Address self, BufferedReader in, ChatBehavior.IOConsumer<String> lineConsumer) {
+        // schedule a message to self
+        scheduler.schedule(() -> self.tell(Poll), 100, MILLISECONDS);
+
+        return ChatBehavior.IOBehavior.of(msg -> {
+            // ignore non-Poll messages
+            if (msg != Poll) return Stay;
+            if (in.ready()) {
+                var input = in.readLine();
                 lineConsumer.accept(input);
             }
-            return Become(lineReader(self, in, lineConsumer));
-        };
-    }
 
+            // "stay" in the same state, ensuring that the initializer is re-evaluated
+            return Become(lineReader(self, in, lineConsumer));
+        });
+    }
 }
