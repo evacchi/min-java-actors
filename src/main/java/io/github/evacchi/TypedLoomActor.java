b78fb771cc4de4fef7f65c9eb89ab4d502badec5
andreaTP <andrea.peruffo1982@gmail.com>
2022-07-12 13:40:22 +0100

    typed loom

--- src/main/java/io/github/evacchi/TypedLoomActor.java
+++ src/main/java/io/github/evacchi/TypedLoomActor.java
@@ -32,29 +32,29 @@ import java.util.function.Function;
 
 import static java.lang.System.out;
 
-public interface LoomActor {
-    interface Behavior extends Function<Object, Effect> {}
-    interface Effect extends Function<Behavior, Behavior> {}
-    interface Address { Address tell(Object msg); }
+public interface TypedLoomActor {
+    interface Effect<T> extends Function<Behavior<T>, Behavior<T>> {}
+    interface Behavior<T> extends Function<T, Effect<T>> {}
+    interface Address<T> { Address<T> tell(T msg); }
 
-    static Effect Become(Behavior like) { return old -> like; }
-    static Effect Stay = old -> old;
-    static Effect Die = Become(msg -> { out.println("Dropping msg [" + msg + "] due to severe case of death."); return Stay; });
+    static <T> Effect<T> Become(Behavior<T> next) { return current -> next; }
+    static <T> Effect<T> Stay() { return current -> current; }
+    static <T> Effect<T> Die() { return Become(msg -> { out.println("Dropping msg [" + msg + "] due to severe case of death."); return Stay(); }); }
 
     record System() {
         private static ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor();
         // DEMO: the third client doesn't work and threads are actively busy
         // private static ExecutorService executorService = Executors.newFixedThreadPool(5); // clientManager + (2 threads X client)
-        public Address actorOf(Function<Address, Behavior> initial) {
-            abstract class RunnableAddress implements Address, Runnable { }
-            var addr = new RunnableAddress() {
-                final LinkedBlockingQueue<Object> mb = new LinkedBlockingQueue<>();
-                public Address tell(Object msg) { mb.offer(msg); return this; }
+        public <T> Address<T> actorOf(Function<Address<T>, Behavior<T>> initial) {
+            abstract class RunnableAddress<T> implements Address<T>, Runnable { }
+            var addr = new RunnableAddress<T>() {
+                final LinkedBlockingQueue<T> mb = new LinkedBlockingQueue<>();
+                public Address<T> tell(T msg) { mb.offer(msg); return this; }
                 public void run() {
-                    Behavior behavior = initial.apply(this);
+                    Behavior<T> behavior = initial.apply(this);
                     while (true) {
                         try {
-                            Object m = mb.take();
+                            T m = mb.take();
                             behavior = behavior.apply(m).apply(behavior);
                         } catch (InterruptedException e) {
                             e.printStackTrace();
