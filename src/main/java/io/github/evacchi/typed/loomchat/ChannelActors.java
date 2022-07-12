db975789dc0d2a50bc7ae3cf77d046d17b0eb862
andreaTP <andrea.peruffo1982@gmail.com>
2022-07-12 14:30:44 +0100

    clean

--- src/main/java/io/github/evacchi/typed/loomchat/ChannelActors.java
+++ src/main/java/io/github/evacchi/typed/loomchat/ChannelActors.java
@@ -23,9 +23,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.UncheckedIOException;
-import java.lang.System;
 import java.net.Socket;
-import java.util.function.BiFunction;
 import java.util.function.Function;
 
 import static io.github.evacchi.TypedLoomActor.*;
